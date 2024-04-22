412f84ebb1ff1712a43330e6d3608fc2dcc35b60
kideng <ki@txzing.com>
2024-04-22 16:26:39 +0800

    feat:登录界面的完善，增加密码可见与不可见逻辑

--- app/src/main/java/com/qq/xuexitong/activity/LoginActivity.kt
+++ app/src/main/java/com/qq/xuexitong/activity/LoginActivity.kt
@@ -2,14 +2,28 @@ package com.qq.xuexitong.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.*
 import com.qq.xuexitong.R
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
+    private val TAG = LoginActivity::class.java.simpleName
     private lateinit var llBack: LinearLayout
+    private lateinit var etUserId: EditText
+    private lateinit var etUserPassword: EditText
+    private lateinit var ivUserPassword: ImageView
+    private lateinit var userForgetPassword: TextView
+    private lateinit var btnLogin: Button
+    private lateinit var tvUserRegister: TextView
+    private lateinit var tvLoginByPhone: TextView
+    private lateinit var tvLoginByOther: TextView
+    private lateinit var loginPolicy: TextView
+    private lateinit var userPolicy: TextView
+    private var isPasswordLock = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +35,37 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun initView() {
         llBack = findViewById(R.id.ll_back)
+        //登录政策
+        loginPolicy = findViewById(R.id.login_policy)
+        loginPolicy.setOnClickListener(this)
+        //用户政策
+        userPolicy = findViewById(R.id.user_policy)
+        userPolicy.setOnClickListener(this)
+        //用户输入的用户名
+        etUserId = findViewById(R.id.et_user_id)
+        //登录按钮
+        btnLogin = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener(this)
+        //新用户注册
+        tvUserRegister = findViewById(R.id.tv_user_register)
+        tvUserRegister.setOnClickListener(this)
+        //手机号快捷登录
+        tvLoginByPhone = findViewById(R.id.tv_login_by_phone)
+        tvLoginByPhone.setOnClickListener(this)
+        //其他登录方式
+        tvLoginByOther = findViewById(R.id.tv_login_by_other)
+        tvLoginByOther.setOnClickListener(this)
+        //忘记密码
+        userForgetPassword = findViewById(R.id.user_forget_password)
+        //设置层级到最上层
+        userForgetPassword.bringToFront()
+        userForgetPassword.setOnClickListener(this)
+        //密码可见与不可见的图标
+        ivUserPassword = findViewById(R.id.iv_user_password)
+        ivUserPassword.bringToFront()
+        ivUserPassword.setOnClickListener(this)
+        //用户输入的密码
+        etUserPassword = findViewById(R.id.et_user_password)
         llBack.setOnClickListener(this)
     }
 
@@ -33,6 +78,38 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         when (v!!.id) {
             R.id.ll_back -> {
                 finish()
+            }
+            R.id.iv_user_password -> {
+                if (isPasswordLock) {
+                    etUserPassword.transformationMethod =
+                        HideReturnsTransformationMethod.getInstance()
+                    ivUserPassword.setImageResource(R.drawable.unlock)
+                } else {
+                    etUserPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                    ivUserPassword.setImageResource(R.drawable.lock)
+                }
+                isPasswordLock = !isPasswordLock
+            }
+            R.id.user_forget_password -> {
+                Log.d(TAG, "用户点击了忘记密码")
+            }
+            R.id.btn_login -> {
+                Log.d(TAG, "用户点击了登录按钮")
+            }
+            R.id.tv_login_by_phone -> {
+                Log.d(TAG, "用户点击了通过手机号码登录")
+            }
+            R.id.tv_user_register -> {
+                Log.d(TAG, "用户点击了新用户注册")
+            }
+            R.id.tv_login_by_other -> {
+                Log.d(TAG, "用户点击了其他登录方式")
+            }
+            R.id.login_policy -> {
+                Log.d(TAG, "用户点击了其他登录政策")
+            }
+            R.id.user_policy -> {
+                Log.d(TAG, "用户点击了其他用户政策")
             }
         }
     }
--- app/src/main/java/com/qq/xuexitong/activity/MainActivity.kt
+++ app/src/main/java/com/qq/xuexitong/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.qq.xuexitong.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -15,6 +16,7 @@ import com.qq.xuexitong.fragment.HomeFragment
 import com.qq.xuexitong.fragment.MeFragment
 import com.qq.xuexitong.fragment.MessageFragment
 import com.qq.xuexitong.fragment.NoteFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListener {
 
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListe
     private lateinit var tvTitle: TextView
     private lateinit var navigation: BottomNavigationBar
     private var clickIndex = 0
+    private var lastClickTime = Calendar.getInstance().timeInMillis
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,30 +42,27 @@ class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListe
         navigation = findViewById(R.id.bv_menu)
         tvTitle = findViewById(R.id.tv_title)
         navigation.setBarBackgroundColor(R.color.white)
-        navigation.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
+        navigation.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
         navigation.setMode(BottomNavigationBar.MODE_SHIFTING);//适应大小
         navigation.addItem(
             BottomNavigationItem(R.drawable.home, "首页").setInactiveIconResource(R.drawable.home)
-                .setActiveColorResource(R.color.blue)
-                .setInActiveColorResource(R.color.gray)
+                .setActiveColorResource(R.color.gray)
         )
             .addItem(
                 BottomNavigationItem(
                     R.drawable.message,
                     "消息"
                 ).setInactiveIconResource(R.drawable.message)
-                    .setActiveColorResource(R.color.blue)
-                    .setInActiveColorResource(R.color.gray)
+                    .setActiveColorResource(R.color.gray)
             )
             .addItem(
                 BottomNavigationItem(R.drawable.note, "笔记").setInactiveIconResource(R.drawable.note)
-                    .setActiveColorResource(R.color.blue)
-                    .setInActiveColorResource(R.color.gray)
+                    .setActiveColorResource(R.color.gray)
+
             )
             .addItem(
                 BottomNavigationItem(R.drawable.me, "我").setInactiveIconResource(R.drawable.me)
-                    .setActiveColorResource(R.color.blue)
-                    .setInActiveColorResource(R.color.gray)
+                    .setActiveColorResource(R.color.gray)
             )
             .setFirstSelectedPosition(0)//默认显示面板
             .initialise()//初始化
@@ -112,6 +112,11 @@ class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListe
     }
 
     override fun onTabSelected(position: Int) {
+        var isQuickClick = false
+        //防止快速点击
+        if (!isInTime()) {
+            isQuickClick = true
+        }
         when (position) {
             0 -> {
                 replaceFragment(fragments[0], fragments[0].tag.toString())
@@ -124,10 +129,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListe
                     tvTitle.text = "消息"
                     clickIndex = 1
                 } else {
-                    navigation.selectTab(clickIndex)
-                    val intent = Intent(this, LoginActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
+                    if (isQuickClick) {
+                        navigation.selectTab(clickIndex)
+                        isQuickClick = false
+                    } else {
+                        navigation.selectTab(clickIndex)
+                        val intent = Intent(this, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    }
                 }
             }
             2 -> {
@@ -136,10 +146,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListe
                     tvTitle.text = "笔记"
                     clickIndex = 2
                 } else {
-                    navigation.selectTab(clickIndex)
-                    val intent = Intent(this, LoginActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
+                    if (isQuickClick) {
+                        navigation.selectTab(clickIndex)
+                        isQuickClick = false
+                    } else {
+                        navigation.selectTab(clickIndex)
+                        val intent = Intent(this, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    }
                 }
             }
             3 -> {
@@ -148,6 +163,20 @@ class MainActivity : AppCompatActivity(), BottomNavigationBar.OnTabSelectedListe
                 clickIndex = 3
             }
         }
+    }
+
+    private fun isInTime(): Boolean {
+        val currentTime = Calendar.getInstance().timeInMillis
+        Log.d(
+            TAG,
+            "currentTime: $currentTime lastTime: $lastClickTime currentTime - lastClickTime: ${currentTime - lastClickTime}"
+        )
+        if (currentTime - lastClickTime < 250) {
+            lastClickTime = currentTime
+            return false
+        }
+        lastClickTime = currentTime
+        return true
     }
 
     override fun onTabUnselected(position: Int) {
