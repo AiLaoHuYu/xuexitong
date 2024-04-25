43fb09918728ef8381bdd3bc71454423160d1dfe
kideng <ki@txzing.com>
2024-04-25 20:50:53 +0800

    feat:修改部分插图，修改detail的Activity为VideoView

--- app/src/main/java/com/qq/xuexitong/activity/VideoDetailActivity.kt
+++ app/src/main/java/com/qq/xuexitong/activity/VideoDetailActivity.kt
@@ -1,17 +1,19 @@
 package com.qq.xuexitong.activity
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.MediaController
+import android.widget.VideoView
 import com.qq.xuexitong.App
 import com.qq.xuexitong.R
-import com.qq.xuexitong.utils.GlideUtil
 
 class VideoDetailActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var imgVideo: ImageView
+    private lateinit var vvVideo: VideoView
     private lateinit var imgUrl: String
     private lateinit var ivBack: ImageView
     private var TAG: String = VideoDetailActivity::class.java.simpleName
@@ -26,10 +28,15 @@ class VideoDetailActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun initView() {
-        imgVideo = findViewById(R.id.img_video)
+        vvVideo = findViewById(R.id.vv_video)
+        vvVideo.setVideoURI(Uri.parse("https://tb-video.bdstatic.com/tieba-smallvideo-transcode-crf/22055766_cf08a0fa3c7a885904a49fc240d8bf5d_51f0620bd7a2_0.mp4?vt=1&pt=3&ver=&cr=2&cd=0&sid=&ft=8&tbau=2024-04-26_2e5b38cfd2934490918c8163859ff82beecb267a24574a5e556a4a81459c3cb7&ptid=8592117458"))
+        val mediaController = MediaController(this)
+        vvVideo.setMediaController(mediaController)
+        vvVideo.start()
         ivBack = findViewById(R.id.iv_back)
+        ivBack.bringToFront()
         ivBack.setOnClickListener(this)
-        GlideUtil().loadImgByGlide(this, imgUrl, imgVideo)
+//        GlideUtil().loadImgByGlide(this, imgUrl, imgVideo)
     }
 
     override fun onClick(v: View?) {
